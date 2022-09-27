b3383f04d6efa704c95bd1626b127355e5cdaf89
許湘潔 <jessiehsu@candyhouse.co>
2022-09-27 14:59:54 +0800

    Initial Commit

---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by 許湘潔 on 2022/9/27.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
